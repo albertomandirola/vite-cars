91152d73b17258bf864dc5c42ddc31d75c058ee9
balbo97 <balbosauro1@gmail.com>
2024-03-07 19:18:22 +0800

    creazione jumbotrone

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+    data() {
+        name: "AppJumbo"
+        return {
+
+        }
+    },
+    props: {
+        
+    },
+    components: {
+        
+    }
+}
+</script>
+
+<template lang="">
+    <div class="jumbotrone">
+
+    </div>
+</template>
+
+<style lang="scss" scoped >
+
+
+.jumbotrone{
+    width: 100%;
+    height: 500px;
+    background-image: url('../../public/img/rolls_royce_black_badge_ghost_car_mountains_snow_nature_background_4k_5k_hd_cars.jpg');
+    background-repeat: no-repeat;
+    background-position: bottom;
+    background-size: cover;
+}
+
+</style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,18 +1,27 @@
 <script>
+import AppJumbo from '../components/AppJumbo.vue';
+
 export default {
-    name: 'AppHome'
+    name: 'AppHome',
+
+    components: {
+        AppJumbo,
+    },
 }
 </script>
 <template lang="">
-    <div class="container py-5">
-            <h1 class="display-5 fw-bold">
-                Benvenuto su Royals Cars
-            </h1>
+    
+    <AppJumbo/>
 
-            <p class="col-md-8 fs-4">Royals Cars semplifica e ottimizza le operazioni quotidiane della tua concessionaria,
-                offrendo strumenti intuitivi per la gestione dell'inventario, delle vendite e del marketing.
-            </p>
-        </div>
+    <div class="container py-5">
+        <h1 class="display-5 fw-bold">
+            Benvenuto su Royals Cars
+        </h1>
+
+        <p class="col-md-8 fs-4">Royals Cars semplifica e ottimizza le operazioni quotidiane della tua concessionaria,
+            offrendo strumenti intuitivi per la gestione dell'inventario, delle vendite e del marketing.
+        </p>
+    </div>
 </template>
 <style lang="">
     
